e73024db7c79e1f9ffeae0caa634817e9dd7ad2d
samiul-bs23 <samiul.islam@brainstation23.com>
2021-10-14 23:11:49 +0600

    UrlBank update

--- domain/models/url_bank.py
+++ domain/models/url_bank.py
@@ -1,10 +1,18 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from domain.models.base_model import BaseModel
 
 
+def get_default_expiration_date():
+    return datetime.now() + timedelta(days=365)
+
+
 class UrlBank(BaseModel):
     actual_url = models.CharField(max_length=3000)
-    actual_url_shortened = models.CharField(max_length=100, db_index=True)
+    expiration_date = models.DateTimeField(default=get_default_expiration_date)
+    md_five_hash = models.CharField(max_length=32, db_index=True)
+    actual_url_shortened = models.CharField(max_length=10, db_index=True)
 
     class Meta:
         db_table = 'url_bank'
